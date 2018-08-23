5e0005bc3101c4519ba3a519b1be3c3e2f52ada4
jimbucktoo <jamesliang.g@gmail.com>
2018-08-23 12:54:12 -0600

    fixed scrollView.

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
+
     @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
 
     @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var countryPicker: UIPickerView!
     
     @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var countryPickerBtn: UIButton!
     
     let states = ["Alaska",
                   "Alabama",
@@ -47,7 +49,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                   "Mississippi",
                   "Montana",
                   "North Carolina",
-                  " North Dakota",
+                  "North Dakota",
                   "Nebraska",
                   "New Hampshire",
                   "New Jersey",
@@ -73,10 +75,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                   "West Virginia",
                   "Wyoming"]
     
+    var countries = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         statePicker.dataSource = self
         statePicker.delegate = self
+        countryPicker.dataSource = self
+        countryPicker.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -95,7 +102,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             print("view.frame.midX: \(view.frame.midX)")
             contentWidth += newX
             
-            
             scrollView.addSubview(imageView)
             if x == 0 {
             imageView.isHidden = true
@@ -103,7 +109,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         contentWidth -= (view.frame.midX * 3)
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height * 1.5)
         print("Count: \(images.count)")
     }
     
@@ -115,22 +121,56 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBAction func stateBtnPressed(_ sender: Any) {
         statePicker.isHidden = false
     }
+    @IBAction func countryBtnPressed(_ sender: Any) {
+        countryPicker.isHidden = false
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        if pickerView == statePicker {
+            print("statePicker used 1.")
+            return 1
+        } else if pickerView == countryPicker{
+            print("countryPicker used 1.")
+            return 1
+        } else {return 1}
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return states.count
+        if pickerView == statePicker {
+            print("statePicker used 2.")
+            return states.count
+        } else if pickerView == countryPicker{
+            print("countryPicker used 2.")
+            return countries.count
+        } else {return 0}
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return states[row]
+        if pickerView == statePicker {
+            print("statePicker used 3.")
+            return states[row]
+        } else if pickerView == countryPicker{
+            print("countryPicker used 3.")
+            return countries[row]
+        } else {return ""}
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        statePickerBtn.setTitle(states[row], for: UIControlState()) //UIControlState.normal
-        statePicker.isHidden = true
+        
+        if pickerView == statePicker {
+            statePickerBtn.setTitle(states[row], for: UIControlState()) //UIControlState.normal
+            statePicker.isHidden = true
+            print("statePicker used 4.")
+        } else if pickerView == countryPicker {
+            countryPickerBtn.setTitle(countries[row], for: UIControlState()) //UIControlState.normal
+            countryPicker.isHidden = true
+            print("countryPicker used 4.")
+        }
+        
+    }
+    
+    @IBAction func btnSubmit(_ sender: Any) {
+        print("Submit.")
     }
     
 }
